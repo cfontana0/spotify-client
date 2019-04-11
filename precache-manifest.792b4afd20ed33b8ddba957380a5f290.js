775e7d4f0715e7b16575fe9153cb7a3811ddf788
Carlos Fontana <cfontana0@gmail.com>
2019-04-11 20:46:40 -0300

    Updates

---
+++ precache-manifest.792b4afd20ed33b8ddba957380a5f290.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "655ce4f26d6496d59f03",
+    "url": "/spotify-client/static/css/main.113f5ad9.chunk.css"
+  },
+  {
+    "revision": "655ce4f26d6496d59f03",
+    "url": "/spotify-client/static/js/main.655ce4f2.chunk.js"
+  },
+  {
+    "revision": "287e36dbdcf47e09a25f",
+    "url": "/spotify-client/static/css/1.91a69751.chunk.css"
+  },
+  {
+    "revision": "287e36dbdcf47e09a25f",
+    "url": "/spotify-client/static/js/1.287e36db.chunk.js"
+  },
+  {
+    "revision": "a1b3384b48d7943829cb",
+    "url": "/spotify-client/static/js/runtime~main.a1b3384b.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/spotify-client/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/spotify-client/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/spotify-client/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/spotify-client/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/spotify-client/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/spotify-client/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/spotify-client/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/spotify-client/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/spotify-client/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/spotify-client/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/spotify-client/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/spotify-client/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/spotify-client/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/spotify-client/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/spotify-client/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/spotify-client/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "5f705a951c4bec34d0515e5644c29a2d",
+    "url": "/spotify-client/index.html"
+  }
+];
