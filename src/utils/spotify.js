709a31b74aae72a8f5b658b3178737c5e156ce69
Carlos Fontana <cfontana0@gmail.com>
2019-04-11 21:26:39 -0300

    Change the Spotify client Id

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -1,13 +1,9 @@
 export const auth = () => {
-  let clientId = 'bfa95165905540d48f188f8a44e93502';
+  const clientId = 'bfa95165905540d48f188f8a44e93502';
   let redirectUri = 'https://cfontana0.github.io/spotify-client/callback';
 
-  if (
-    window.location.hostname === 'localhost' ||
-    window.location.hostname === '127.0.0.1'
-  ) {
+  if (window.location.hostname === 'localhost') {
     redirectUri = 'http://localhost:3000/callback';
-    clientId = '860725a4e08640199e5261d2a8a5d07c';
   }
 
   window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=${redirectUri}`;
